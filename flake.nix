f8bcc9229a7793d6b2eb7d32a856bb56143cda3d
Cody Gunton <codygunton@gmail.com>
2026-05-01 04:32:50 -0400

    nix: pin upstream sources via flake inputs

--- flake.nix
+++ flake.nix
@@ -4,9 +4,33 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+
+    # Pinned upstream sources. Same commits/tags as the deleted
+    # docker/versions.txt; flake.lock is now the audit surface.
+    sail-src = {
+      url = "github:rems-project/sail/277470b2ab472af8064e1295be5573845239c96b";
+      flake = false;
+    };
+    sail-riscv-src = {
+      url = "github:riscv/sail-riscv/04e595954b2af7fa9e18f2519e4ae4bba72b2701";
+      flake = false;
+    };
+    zisk-src = {
+      url = "github:0xPolygonHermez/zisk/b3ca745b80423c8123dc24f19039865a5bc8b074";
+      flake = false;
+    };
+    pil2-compiler-src = {
+      url = "github:0xPolygonHermez/pil2-compiler/v0.8.0";
+      flake = false;
+    };
+    pil2-proofman-src = {
+      url = "github:0xPolygonHermez/pil2-proofman/v0.15.0";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, sail-src, sail-riscv-src,
+              zisk-src, pil2-compiler-src, pil2-proofman-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
