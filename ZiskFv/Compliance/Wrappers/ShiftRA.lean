cef7ea5571e1056dc29cad9765aaab7aee9f5bf0
Cody Gunton <codygunton@gmail.com>
2026-06-05 14:22:18 -0400

    Close OpEnvelope Aeneas bridge evidence gap (#58)

--- ZiskFv/Compliance/Wrappers/ShiftRA.lean
+++ ZiskFv/Compliance/Wrappers/ShiftRA.lean
@@ -32,22 +32,10 @@ lemma equiv_SRAW
     (providerRow : Array FGL)
     (r_main : ℕ)
     (bus : ZiskFv.Compliance.BusRows)
-    (h_input_r1_sail : read_xreg (regidx_to_fin r1) state
-      = EStateM.Result.ok sraw_input.r1_val state)
-    (h_input_r2_sail : read_xreg (regidx_to_fin r2) state
-      = EStateM.Result.ok sraw_input.r2_val state)
-    (h_input_rd : sraw_input.rd = regidx_to_fin rd)
-    (h_input_pc : state.regs.get? Register.PC = .some sraw_input.PC)
-    (h_exec_len : bus.exec_row.length = 2)
-    (h_e0_mult : bus.exec_row[0]!.multiplicity = -1)
-    (h_e1_mult : bus.exec_row[1]!.multiplicity = 1)
-    (h_nextPC_matches :
-      (register_type_pc_equiv ▸ (BitVec.ofNat 64 (bus.exec_row[1]!.pc).val))
-        = (PureSpec.execute_RTYPE_sraw_pure sraw_input).nextPC)
-    (h_m0_mult : bus.e0.multiplicity = -1) (h_m0_as : bus.e0.as.val = 1)
-    (h_m1_mult : bus.e1.multiplicity = -1) (h_m1_as : bus.e1.as.val = 1)
-    (h_m2_mult : bus.e2.multiplicity = 1) (h_m2_as : bus.e2.as.val = 1)
-    (h_rd_idx : sraw_input.rd = Transpiler.wrap_to_regidx bus.e2.ptr)
+    (promises : ZiskFv.EquivCore.Promises.RTypePromises
+        state sraw_input.r1_val sraw_input.r2_val sraw_input.rd sraw_input.PC
+        (PureSpec.execute_RTYPE_sraw_pure sraw_input).nextPC
+        r1 r2 rd bus.exec_row bus.e0 bus.e1 bus.e2)
     (pins : ZiskFv.Compliance.MainRowPins m r_main 1 ZiskFv.Trusted.OP_SRA_W)
     (h_component :
       providerTable.component = ZiskFv.AirsClean.BinaryExtension.shiftStaticLookupComponent)
@@ -78,21 +66,7 @@ lemma equiv_SRAW
       h_component h_table_spec h_provider_row
   exact ZiskFv.EquivCore.Sraw.equiv_SRAW_of_static_row state sraw_input r1 r2 rd
     m row r_main bus
-    { input_r1_eq := h_input_r1_sail
-      input_r2_eq := h_input_r2_sail
-      input_rd_eq := h_input_rd
-      input_pc_eq := h_input_pc
-      exec_len := h_exec_len
-      e0_mult := h_e0_mult
-      e1_mult := h_e1_mult
-      nextPC_matches := h_nextPC_matches
-      m0_mult := h_m0_mult
-      m0_as := h_m0_as
-      m1_mult := h_m1_mult
-      m1_as := h_m1_as
-      m2_mult := h_m2_mult
-      m2_as := h_m2_as
-      rd_idx := h_rd_idx }
+    promises
     pins h_match h_shift_facts.1 h_shift_facts.2
     (by simpa [row] using h_input_r1_row)
     (by simpa [row] using h_shift_pin_row)
